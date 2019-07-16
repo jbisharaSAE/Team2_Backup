086ef1c249f6b4d8eb467884385ac90e284f9353
jbisharaSAE <44623870+jbisharaSAE@users.noreply.github.com>
2019-07-16 14:48:13 +1000

    Minor Changes / Setting up PC version

--- Assets/James/ScenesJames/Scripts/BalloonFDefence/PCCrossBowShoot.cs
+++ Assets/James/ScenesJames/Scripts/BalloonFDefence/PCCrossBowShoot.cs
@@ -8,15 +8,16 @@ namespace Nokobot.Assets.Crossbow
     {
         public GameObject arrowPrefab;
         public Transform arrowLocation;
-        public GameObject myCircle;
-        public ScoreManager myScoreScript;
+
+
 
         private float shootGap = 0.4f;
         private float timer;
+
         public float shotPower = 100f;
-        private static OVRInput.Controller myController = OVRInput.Controller.RTrackedRemote;
-        private LayerMask layerMask = 1 << 10;
-        private bool specialAbility;
+
+        
+
 
         void Start()
         {
@@ -41,54 +42,14 @@ namespace Nokobot.Assets.Crossbow
 
             }
 
-            Ray ray;
-
-            RaycastHit hit;
-
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
-                {
-                    if (myScoreScript.progressRatio >= 100f)
-                    {
-                        if (Input.GetMouseButtonDown(1))
-                        {
-                            specialAbility = true;
-                        }
-                        if (Input.GetMouseButtonUp(1))
-                        {
-                            specialAbility = false;
-                        }
-                    }
-
-
-
-                }
 
             
            
 
-            SpecialArrows(specialAbility, hit);
-            
+           
         }
         
-        private void SpecialArrows(bool active, RaycastHit objHit)
-        {
-         //   print("Hi There");
-            if (active)
-            {
-                myCircle.SetActive(true);
-                myCircle.transform.position = objHit.point;
-                //if (Input.GetMouseButtonDown(1))
-                    //myCircle
-            }
-            else
-            {
-                myCircle.SetActive(false);
-            }
-            
-        }
+        
 
     }
 }
