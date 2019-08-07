5ee2280263d5a69cd72beb63d34a81f1012c745c
luc holland <44623852+Exotic-Deer-Arts@users.noreply.github.com>
2019-08-07 20:18:12 +1000

    added powerup despawner

---
+++ Assets/James/ScenesJames/Scripts/BalloonFDefence/DestroyOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+
+    public GameObject objToDestroy;
+    public GameObject effect;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "powerup")
+            Instantiate(effect, objToDestroy.transform.position, objToDestroy.transform.rotation);
+        Destroy(objToDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
