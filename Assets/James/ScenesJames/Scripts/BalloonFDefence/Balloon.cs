a3ed7e23b8190efe041b6fb68e05e633ceb5a3c1
jbisharaSAE <44623870+jbisharaSAE@users.noreply.github.com>
2019-08-07 18:20:46 +1000

    Update Balloon.cs

--- Assets/James/ScenesJames/Scripts/BalloonFDefence/Balloon.cs
+++ Assets/James/ScenesJames/Scripts/BalloonFDefence/Balloon.cs
@@ -161,7 +161,7 @@ public class Balloon : MonoBehaviour
 
                     parentObj.SendMessage("StartRunning");
 
-                    Destroy(parentObj);
+                    //Destroy(parentObj);
                     break;
 
             }
