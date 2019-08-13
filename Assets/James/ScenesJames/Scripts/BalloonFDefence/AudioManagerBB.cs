f3bbfb7090930d3dc8b6969401ed0c9186bb5a4b
jbisharaSAE <44623870+jbisharaSAE@users.noreply.github.com>
2019-08-13 23:32:42 +1000

    small initialisation with audio setup

---
+++ Assets/James/ScenesJames/Scripts/BalloonFDefence/AudioManagerBB.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerBB : MonoBehaviour
+{
+    public AudioClip gameStart;
+    public AudioClip slowTime;
+    public AudioClip hpRegen;
+    public AudioClip rapidFire;
+    public AudioClip metalHit;
+    public AudioClip balloonHit;
+    public AudioClip crossBowFire;
+    public AudioClip crossBowReload;
+    public AudioClip gateAttack;
+
+    private AudioSource audioPlayerSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayerSource = GetComponent<AudioSource>();
+    }
+
+    
+}
