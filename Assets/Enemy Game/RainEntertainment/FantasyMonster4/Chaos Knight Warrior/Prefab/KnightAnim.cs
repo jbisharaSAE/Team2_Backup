a3cb4bda1587c9df9ebb1c408cc89e51c2eca5c8
luc holland <44623852+Exotic-Deer-Arts@users.noreply.github.com>
2019-08-21 22:50:34 +1000

    started animation for final enemy (Not finished yet

---
+++ Assets/Enemy Game/RainEntertainment/FantasyMonster4/Chaos Knight Warrior/Prefab/KnightAnim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class KnightAnim : MonoBehaviour
+{
+    private NavMeshAgent chaos;
+    private Animator KnightAn;
+    public bool KnightRunning;
+            
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chaos = GetComponent<NavMeshAgent>();
+
+        target = GameObject.FindGameObjectWithTag("MyTarget");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        chaos.SetDestination(target);
+
+    }
+}
